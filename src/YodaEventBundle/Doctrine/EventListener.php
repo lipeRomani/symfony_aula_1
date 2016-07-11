64ec072f9a5c7cf17f5cb8b966deda4f1ab32bab
Felipe Romani <felipe.romani@wideti.com.br>
2016-07-11 16:48:12 -0300

    adicinado event listener doctrine na entity event

---
+++ src/YodaEventBundle/Doctrine/EventListener.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: romani
+ * Date: 11/07/16
+ * Time: 16:14
+ */
+
+namespace YodaEventBundle\Doctrine;
+
+
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+
+class EventListener
+{
+    public function prePersist(LifecycleEventArgs $args){
+
+    }
+}
